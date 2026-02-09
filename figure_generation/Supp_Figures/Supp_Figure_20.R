cd087319efbdd614c8a1d5ed236772984ef94b9a
GTOP-Network <gtop-contact@smart.org.cn>
2026-02-09 13:48:26 +0800

    Add files via upload

--- figure_generation/Supp_Figures/Supp_Figure_20.R
+++ figure_generation/Supp_Figures/Supp_Figure_20.R
@@ -1,50 +1,67 @@
 #==============================================#
-# S20 Cumulative PVE #
+# tuQTL & tuVariants #
 # Supp-Figure-20#
 #==============================================#
 
-library(data.table)
-library(dplyr)
+library(patchwork)
+library(ggplot2)
 library(ggpubr)
-library(cowplot)
-
+library(ggbreak)
 setwd("/media/london_A/mengxin/GTOP_code/supp/supp_fig20")
 
+# Supp.Fig.20a left torus result of known/novel tuQTL ---------------------
+order <- rev(c("enhancer","promoter","open chromatin region","CTCF binding site","TF binding site","3 prime UTR","5 prime UTR","frameshift","intron","missense","NC transcript","splice acceptor","splice donor","splice region","stop gained", "synonymous"))
+plotdf <- fread("./input/Figure S20a.left.txt")
 
-# Supp.Fig.20 Cumulative PVE-------------------------------------------------
-
-
-df <- fread("./input/Figure S20.txt")
-figlist <- list()
-for (qtltype_t in unique(df$qtltype)) {
-  plotdf <- df %>% dplyr::filter(qtltype==qtltype_t)
-  
-  plotdf$tissue <- factor(plotdf$tissue, levels = plotdf$tissue)
-  
-  
-  scale_factor <- max(plotdf$num_PCs) / max(plotdf$Cumulative_PVE)
-  
-  figlist[[qtltype_t]] <- ggplot(plotdf, aes(x = tissue)) +
-    geom_bar(aes(y = Cumulative_PVE, fill = "pro_explain"), 
-             stat = "identity") +
-    geom_point(aes(y = num_PCs / scale_factor, color = "PCnumber"),
-               shape = 19) +
-    scale_y_continuous(
-      name = "Proportion of expression variance captured by the PCs",
-      limits = c(0, 0.8),
-      sec.axis = sec_axis(~. * scale_factor, 
-                          name = "Number of PCs in xQTL mapping")
-    ) +
-    scale_fill_manual(values = c("pro_explain" = "#c9c9c9")) +
-    scale_color_manual(values = c("PCnumber" = "#9d3929")) +
-    theme_pubr()+
-    theme(axis.text.x = element_blank(),
-          legend.position = "none")+
-    ggtitle(qtltype_t)
-}
-
-cowplot::plot_grid(plotlist = figlist, ncol = 1)
+p1 <- plotdf %>%
+  mutate(Ann=factor(Ann, levels=order)) %>% 
+  ggplot(.) +
+  geom_pointrange(aes(x=Ann, y = logmFC, ymin=lFC, ymax=hFC, color = QTL, shape=QTL), 
+                  position=position_dodge(width=0.8), size = 0.5)+
+  scale_color_manual(values=rev(c("tuQTL novel"="#20407b" , "tuQTL known"="#8892ae")))+
+  scale_shape_manual(values = c("tuQTL novel"=16, "tuQTL known"=15))+
+  geom_hline(yintercept=0, linetype="dashed", color = "red")+
+  # ylim(-8,20)+
+  ylab(expression("Log"[2]*"(Fold Enrichment)"))+
+  coord_flip()+
+  theme_pubr(legend = "right")+
+  theme(
+    axis.text.x = element_text(color="black"),
+    axis.text.y = element_text(color="black"),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank())
+p1
 
 
+# Supp.Fig.20a right known or novel tuVariants proportion -----------------
 
 
+plotdf <- fread("./input/Figure S20a.right.txt")
+
+p2 <- plotdf %>% 
+  mutate(feature=factor(feature, levels=order)) %>% 
+  ggplot(.)+
+  geom_bar(aes(x=meanratio, y=feature, fill=type), stat = "identity", position=position_dodge(width=0.9))+
+  geom_errorbar(aes(xmax=meanratio+sdratiio, xmin=meanratio-sdratiio, y=feature, group=type), position=position_dodge(width=0.9),width=0)+
+  scale_fill_manual(values=rev(c("novel"="#20407b" , "known"="#8892ae")))+
+  # scale_x_break(c(0.1,0.70),
+  #               space = 0.3,
+  #               scales = 1.5)+
+  theme_pubr(legend = "right")+
+  xlab("Proportion of variants")+
+  ylab("")
+p2
+
+
+# Supp.Fig.20b distribution of credible set variants relative to s --------
+
+density_df <- fread("./input/Figure S20b.txt")
+
+density_df %>% 
+  ggplot(., aes(x = pos_index, color=qtltype)) +
+  geom_density( alpha = 0.5, adjust = 0.5) +
+  labs(x = "Position", y = "Density of sQTL") +
+  scale_color_manual(values = c("SV" ="#477980" , "TR" = "#9d3928", "Small_variant" = "#7d8bad"))+
+  geom_vline(xintercept = 50, linetype = "dashed", color = "black", alpha = 0.5) +
+  geom_vline(xintercept = 71, linetype = "dashed", color = "black", alpha = 0.5) +
+  theme_pubr()
