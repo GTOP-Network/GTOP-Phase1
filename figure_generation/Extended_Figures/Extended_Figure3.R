af1dc9f1bd8da81e01ffcef5158d49f4719f14e8
GTOP-Network <gtop-contact@smart.org.cn>
2026-02-09 09:12:06 +0800

    Add files via upload

--- figure_generation/Extended_Figures/Extended_Figure3.R
+++ figure_generation/Extended_Figures/Extended_Figure3.R
@@ -3,26 +3,26 @@
 # Extended-Figure-3#
 #==============================================#
 
+
 library(dplyr)
 library(data.table)
 library(ggplot2)
 library(ggpubr)
 
-setwd("/media/london_A/mengxin/GTOP_code/extend/extend_3/input")
-
-# Extend.Data.Fig.3a ASE & ASTS ----------------------------------------------
-
-df_plot <- fread("Exfig 3a.txt")
-reg <- lm(formula = sig.ase ~ total.ase,data=df_plot)
+setwd("/media/london_A/mengxin/GTOP_code/extend/extend_3")
+# Extended.Fig.3ab ASE & ASTS ----------------------------------------------
+df_plot <- fread("./input/Exfig 3a.txt")
+reg <- lm(formula = sig ~ total,data=df_plot)
 coeff <- coefficients(reg)
 intercept <- coeff[1]
 slope <- coeff[2]
-p1 <- ggplot(df_plot,aes(x=total.ase,y=sig.ase,color=Abbreviation)) + geom_point(size=3) + theme_pubr() + 
+p1 <- ggplot(df_plot,aes(x=total,y=sig,color=Abbreviation)) + geom_point(size=3) + theme_pubr() + 
   scale_color_manual(breaks = df_plot$Abbreviation,values = paste0("#",df_plot$Tissue_Color_Code)) + 
   xlab("Number of genes tested") + ylab("Number of significant genes") + 
   geom_abline(intercept = intercept,slope = slope,color="black",linetype="dashed",size=1.5)
-
-
+p1
+#Extended.fig.3b
+df_plot <- fread("./input/Exfig 3b.txt")
 reg <- lm(formula = sig.asts ~ total.asts,data=df_plot)
 coeff <- coefficients(reg)
 intercept <- coeff[1]
@@ -35,10 +35,18 @@ p2 <- ggplot(df_plot,aes(x=total.asts,y=sig.asts,color=Abbreviation)) + geom_poi
 cowplot::plot_grid(p1,p2,align = "h",ncol=2)
 
 
-# Extend.Data.Fig.3b ---------------------------------------------------------
+# Extended.fig.3c  ---------------------------------------------------------
+df_plot <- fread("./input/Exfig 3c.txt")
+ggplot(df_plot,aes(x=Var1,y=Freq)) + 
+  geom_bar(stat = "identity",width=.6, fill="#6c81b1") + 
+  xlab("# transcripts per gene")+
+  ylab("# Genes")+
+  theme_pubr()
 
 
-df_sample.l <- fread("Exfig 3b.txt") %>% 
+# Extended.fig.3d  ---------------------------------------------------------
+
+df_sample.l <- fread("./input/Exfig 3d.txt") %>% 
   mutate(Var2=as.character(Var2))
 p3 <- ggplot(df_sample.l,aes(x=Var1,y=Freq,fill=Var2)) + geom_bar(stat = "identity",width=.8) + theme_pubr() + 
   theme(axis.text.x = element_text(angle = 90,hjust=1,vjust=.5)) + xlab("")+ylab("# ASE events")+
@@ -46,26 +54,14 @@ p3 <- ggplot(df_sample.l,aes(x=Var1,y=Freq,fill=Var2)) + geom_bar(stat = "identi
   scale_fill_manual(breaks = c("0","1"),labels=c("non-significant events", "significant events"),values = c("grey","#4380b8"))+
   theme(axis.text.x = element_blank())
 
-
-#  Extend.Data.Fig.3c --------------------------------------------------------
-
-
-df_sample <- fread("Exfig 3c.txt") %>% 
+# Extended.fig.3e  ---------------------------------------------------------
+df_sample <- fread("./input/Exfig 3e.txt") %>% 
   mutate(Var2=as.character(Var2))
 p4 <- ggplot(df_sample,aes(x=Var1,y=Freq,fill=Var2)) + geom_bar(stat = "identity",width=.8) + theme_pubr() + 
   theme(axis.text.x = element_text(angle = 90,hjust=1,vjust=.5)) + xlab("")+ylab("# ASTS events")+
   scale_y_log10() + 
   scale_fill_manual(breaks = c("0","1"), labels=c("non-significant events", "significant events"),values = c("grey","#4380b8"))+
   theme(axis.text.x = element_blank())
-
-# Extend.Data.Fig.3d ---------------------------------------------------------
-
-df_plot <- fread("Exfig 3d.txt")
-ggplot(df_plot,aes(x=Var1,y=Freq)) + 
-  geom_bar(stat = "identity",width=.6, fill="#6c81b1") + 
-  xlab("# transcripts per gene")+
-  ylab("# Genes")+
-  theme_pubr()
 
 
 
