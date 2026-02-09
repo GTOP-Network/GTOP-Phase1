43562654ea04c31006bad671befc82b900af1ca4
GTOP-Network <gtop-contact@smart.org.cn>
2026-02-09 11:41:18 +0800

    Add files via upload

--- figure_generation/Supp_Figures/Supp_Figure_15.R
+++ figure_generation/Supp_Figures/Supp_Figure_15.R
@@ -1,81 +1,78 @@
+
+
 #==============================================#
-#  ASE ASTS#
+#  #isolaser resault #
 # Supp-Figure-15#
 #==============================================#
-
-library(dplyr)
 library(data.table)
+library(dplyr)
+library(magrittr)
 library(tidyverse)
-library(ggplot2)
+library(magrittr)
+library(dplyr)
 library(ggpubr)
-library(ggrastr)
-
-setwd("/media/london_A/mengxin/GTOP_code/supp/supp_fig15/input")
-
-
-#Supp.Fig.15a the number of significant ase/asts -------------------------------
-
-df_plot.m <- fread("Figure S15a.txt")
-ggplot(df_plot.m,aes(x=group,y=Count,fill=Significance)) + geom_bar(stat="identity",width=.8) + theme_pubr() + 
-  scale_fill_manual(breaks = c("no","yes"),values = c("#D3DCE5","#C0B5D7"))
+library(patchwork)
+setwd("/media/london_A/mengxin/GTOP_code/supp/supp_fig15")
 
 
-# Supp.Fig.15b comparation bewteen SRS ASE and LRS ASE
-df_plot <- fread("Figure S15b.txt")
-ggplot(df_plot)+
-  # geom_point(size=3, aes(x=logafc, y=as.numeric(log2_aFC)), alpha=0.6, color="grey80") +
-  geom_point_rast(size=3, aes(x=logafc, y=as.numeric(log2_aFC)), alpha=0.6, color="#5d86af") +
-  xlab("LRS log2aFC") + ylab("SRS log2aFC") +
-  theme_pubr()
+# Supp.Fig.15a  Number of unique cis-directed events and the numbe --------
 
-# Supp.Fig.15c concordance of ASE direction between LRS and SRS across thresholds
+df_count_by_sample <- fread("./input/Figure S15a.txt")
+df_count_long <- melt(df_count_by_sample,id.vars = c("sample","donorID","Tissue","Tissue_Color_Code"))
+df_count_long$sample <- factor(df_count_long$sample,levels = df_count_by_sample$sample)
 
-df_plot <- fread("Figure S15c.txt")
+p1 <- ggplot(df_count_long,aes(x=sample,y=value,fill=variable)) + 
+  geom_bar(stat = "identity",width=.8,position = position_dodge()) + 
+  geom_hline(yintercept = c(median(df_count_by_sample$eventCount),median(df_count_by_sample$geneCount)),linetype="dashed") +
+  xlab("Number of events/genes")+
+  theme_pubr() + 
+  scale_fill_manual(
+    values = c(eventCount = "#b9aece", geneCount = "#1d76b3")) +
+  theme(
+    axis.title.x = element_blank(),
+    axis.text.x  = element_blank(),
+    axis.ticks.x = element_blank()
+  ) + ylab("Count");p1
 
-ggplot(df_plot)+
-  geom_point(aes(x=thresh, y=ratio, fill=dir, color=dir))+
-  geom_line(aes(x=thresh, y=ratio, fill=dir, color=dir))+
-  scale_fill_manual(values=c("diff"="grey", "same"="#d8bbb3"))+
-  scale_color_manual(values=c("diff"="grey", "same"="#d8bbb3"))+
-  scale_y_continuous(limits = c(0, 1))+
-  theme_pubr()+
-  xlab("Threshold of ASE")+
-  ylab("Proportion of significanct ASE")
+df_count_by_sample$sample <- factor(df_count_by_sample$sample,levels = df_count_by_sample$sample)
+p2 <- ggplot(df_count_by_sample,aes(x=sample,y=1,fill=Tissue)) + geom_tile() +
+  theme_void() + scale_fill_manual(breaks = df_count_long$Tissue,values = paste0("#",df_count_long$Tissue_Color_Code)) + 
+  theme(legend.position = "none");p2
+library(patchwork)
 
-
-# Supp.Fig.15d the distribution of ASE/ASTS events for donors
-df_plot <- fread("Figure S15d.txt")
-ggplot(df_plot,aes(x=count,y=Freq,color=group)) + 
-  scale_color_manual(
-  values = c(
-    "ASE" = "#bcb3d1",
-    "ASTS" = "#a4c48a"
-  ))+
-    geom_point(size=3) + theme_pubr()
-
-# Supp.Fig.15e log2aFC in ASE events for donor 1 and donor 2
-
-df_plot <- fread("Figure S15e.txt")
-ggplot(df_plot,aes(x=logafc.x,y=logafc.y)) + geom_point(color="steelblue") + 
-  xlab("log aFC in donor-1")+
-  ylab("log aFC in donor-2")+
-  theme_pubr()
+p1 / p2  + plot_layout(heights = c(5, 0.5))
 
 
-# S15 f-g the distribution of ASE/ASTS genes
+###Fig. S16b  Number of variants, exonic parts, and genes detected by mergingsamples of the same tissue type using isoLASER-joint
 
-df_plot <- fread("Figure S15f-g.txt")
 
-p1 <- ggplot(df_plot,aes(x=ase_tissues))+
-  geom_bar(aes(y = after_stat(count / 1000)), stat = "count")+
-  xlab("Number of tissues")+
-  ylab("Number of ASE genes (x10³)")+
-  theme_pubr()
+df_count_m <- fread("./input/Figure S15b.txt")
 
-p2 <- ggplot(df_plot,aes(x=asts_tissues))+
-  geom_bar(aes(y = after_stat(count / 1000)), stat = "count")+
-  xlab("Number of tissues")+
-  ylab("Number of ASTS genes (x10³)")+
-  theme_pubr()
+df_count_m$Tissue <- factor(df_count_m$Tissue,levels=unique(df_count_m$Tissue))
 
-cowplot::plot_grid(p1, p2, ncol = 2)
+p3 <- ggplot(df_count_m,aes(x=Tissue,y=value,fill=variable)) + 
+  geom_bar(stat = "identity",width=.8,position = position_dodge()) + 
+  geom_hline(yintercept = c(median(df_count_m$Count_Variants),median(df_count_m$Count_exon),median(df_count_m$Count_gene)),
+             linetype="dashed")  + theme_pubr() + 
+  scale_fill_manual(
+    values = c(Count_Variants = "#deebf7", Count_exon = "#9ecae1",Count_gene="#3182bd")) +
+  theme(
+    axis.title.x = element_blank(),
+    axis.text.x  = element_blank(),
+    axis.ticks.x = element_blank()
+  ) + ylab("Count");p3
+
+
+color <- df_count_m %>% distinct(Tissue, Tissue_Color_Code)
+
+
+p4 <- ggplot(color,aes(x=Tissue,y=1,fill=Tissue)) + geom_tile() +
+  theme_void() + 
+  scale_fill_manual(breaks = color$Tissue,
+                    values = setNames(paste0("#",color$Tissue_Color_Code),
+                                      color$Tissue)) + 
+  theme(legend.position = "none");p4
+
+
+p3 / p4  + plot_layout(heights = c(5, 0.5))
+
