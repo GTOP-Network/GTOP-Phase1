4d9fa31c4250607a72c8ddcf3767e3b296aac64e
GTOP-Network <gtop-contact@smart.org.cn>
2026-02-09 14:32:19 +0800

    Add files via upload

--- figure_generation/Supp_Figures/Supp_Figure_23.R
+++ figure_generation/Supp_Figures/Supp_Figure_23.R
@@ -1,43 +1,65 @@
 #==============================================#
-# ASE eQTL correlation #
+# GTOP-GTEX-eQTL-correlation-SNV #
 # Supp-Figure-23#
 #==============================================#
-
 library(ggplot2)
+library(tidyverse)
 library(ggpubr)
 library(data.table)
-library(tidyverse)
-library(cowplot)
+
 setwd("/media/london_A/mengxin/GTOP_code/supp/supp_fig23")
-# Supp.Fig.23 -------------------------------------------------------------
-
-df_plot <- fread("./input/Figure S23.txt", sep = "\t")
+# Supp.Fig.24a correlation of eQTL effects between GTOP and GTEx -----------
 
 
+plotdf <- fread("./input/Figure S23a.txt")
 figlist <- list()
-for (tis in unique(df_plot$tissue)) {
-  df_tmp <- df_plot %>% dplyr::filter(tissue==tis)
-  cor_test <- cor.test(df_tmp$slope, df_tmp$logafc)
-  rho <- cor_test$estimate[1]
-  p_value <- cor_test$p.value[1]
-
+for (tis in unique(plotdf$tissue_gtop)) {
+  plotdf_t <- plotdf %>% dplyr::filter(tissue_gtop==tis)
   
   
-  figlist[[tis]] <- ggplot(df_tmp)+
-    geom_point(aes(x=logafc,y=slope),
-               fill="grey", size=2.5, shape=21)+
-    geom_text(
-      aes(x=0, y=max(slope)-0.5),
-      label = paste("rho =", round(rho, 2), ", p =", format(p_value, scientific = TRUE, digits = 3)),
-      hjust = 1, vjust = 1, size = 3, color = "black")+
-    geom_smooth(aes(x = logafc, y = slope),
-                method = "lm", se = FALSE,
-                color = "black", size = 0.5, linetype = "dashed") +
-    xlab("ASE aFC") + 
-    ylab("Tensorqtl slope")+
-    theme_pubr()+
-    ggtitle(sprintf("%s", tis))
-  
+  figlist[[tis]] <- ggplot(plotdf_t, aes(x=slope_gtop, y=slope_gtex)) + #zscore
+    geom_point(color="#cb9b5e", size=1)+xlab("Slope of eQTL from GTOP")+ylab("Slope of eQTL from GTEx")+
+    geom_vline(xintercept = 0, "black")+
+    geom_hline(yintercept = 0, "black")+
+    stat_cor(method = "pearson", label.x =-3, label.y = 3)+
+    scale_x_continuous(limits = c(-4, 4))+
+    scale_y_continuous(limits = c(-4, 4))+
+    ggtitle(sprintf("%s", tis))+
+    theme_bw()+
+    theme(panel.grid = element_blank(), aspect.ratio = 1)
 }
 
-plot_grid(plotlist = figlist, cols = 3)
+
+cowplot::plot_grid(plotlist = figlist, ncol = 3)
+
+
+# Supp.Fig.24b rb of eQTL between GTOP and GTEx -----------
+
+df <- readRDS("input/Figure S23b.rds")
+
+ggplot(df, aes(x = tis1, y = tis2, fill = r_b)) +
+  geom_tile(color = "white", linewidth = 0.5) +
+  geom_text(aes(label = label), 
+            size = 3, 
+            color = "black") +
+  scale_fill_gradient2(
+    high = "#2171B5",
+    mid = "#9ECAE1",
+    low = "white",
+    midpoint = median(df$r_b, na.rm = TRUE),
+    name = "Rb"
+  )+
+  labs(
+    x = "Discovery",
+    y = "Replication",
+  ) +
+  theme_pubr() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),
+    axis.text.y = element_text(hjust = 1),
+    panel.grid = element_blank(),
+    plot.title = element_text(hjust = 0.5, face = "bold"),
+    legend.position = "right"
+  ) +
+  coord_fixed(ratio = 1)
+
