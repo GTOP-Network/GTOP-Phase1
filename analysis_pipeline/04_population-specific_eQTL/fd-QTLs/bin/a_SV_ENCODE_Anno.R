98eee79c7f335ec308a72ef6aa9f05adfae96b35
GTOP-Network <gtop-contact@smart.org.cn>
2026-02-14 14:06:04 +0800

    Delete analysis_pipeline/04_population-specific_eQTL/fd-QTLs/bin/a_SV_ENCODE_Anno.R

--- analysis_pipeline/04_population-specific_eQTL/fd-QTLs/bin/a_SV_ENCODE_Anno.R
+++
@@ -1,8 +0,0 @@
-
-library(data.table)
-
-encode_data <- fread("/media/bora_A/zhangt/src/data/ENCODE_cCREs_regulation_UCSC") #42126344
-
-encode_cleandata <- encode_data[, .(`#Chrom`=`#chrom`, Start=chromStart, End=chromEnd, cCRE=encodeLabel)]
-encode_cleandata$`#Chrom` <- gsub("chr", "", encode_cleandata$`#Chrom`)
-fwrite(encode_cleandata, "../src/bin/AnnotSV/share/AnnotSV/Annotations_Human/Users/GRCh38/AnyOverlap/ENCODE_cCREs.sorted.bed", sep = "\t")
